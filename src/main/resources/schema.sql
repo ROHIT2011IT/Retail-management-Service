3a7ff994a9303f8bea405cf77f36eae1f0061b94
Rohit Garg <47842@hexaware.com>
2019-08-14 21:38:24 +0530

    new branch created

--- src/main/resources/schema.sql
+++
@@ -1,3 +0,0 @@
---create table StoreProduct (store_id bigint, product_id bigint, qty bigint);
-create table Role (role_id int, role varchar(50));
---create table OrderProduct(order_productID bigint, productID bigint, qty int);
